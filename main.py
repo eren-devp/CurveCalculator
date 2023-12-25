b9873b15058f315dcf4a6144b1ce39400786a0b9
Talha <143719883+eren-devp@users.noreply.github.com>
2023-12-25 21:21:36 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ sum = 0
 for i in grades:
     try:
         sum += float(i)
-    except:
+    except: # If we are not able to convert it to float then we will get a error massage. (NA)
         pass
 
-print(f"Average: {sum/len(grades)}")
+print(f"Average: {sum / len(grades)}")
