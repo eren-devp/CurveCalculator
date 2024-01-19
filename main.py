85c571e107d558a7b559602668b4a3bd36569326
Talha <143719883+eren-devp@users.noreply.github.com>
2024-01-19 19:16:48 +0300

    Update main.py
    Added multiple input choice

--- main.py
+++ main.py
@@ -1,10 +1,26 @@
+input_amount = int(input("Input count: "))
+input_percentages = [100]
+if input_amount != 1:
+    input_percentages = []
+    for i in range(input_amount):
+        input_percentages.append(float(input(f"Enter {i+1}. input's percentage: ")))
+
 grades = open("grades.txt").read().split("\n")
 
 sum_grades = 0
 na_count = 0
 for i in grades:
     try:
-        sum_grades += float(i)
+        current_sum = 0
+        i = i.split("\t")
+        number = 0
+
+        for j in i:
+            current_sum += float(j) * float(input_percentages[number] / 100.0)
+            number += 1
+
+        sum_grades += current_sum
+
     except:
         na_count += 1
 
